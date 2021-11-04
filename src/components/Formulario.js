544a4110626afd739426c7c0c861ae8cba9f33bb
Dusknior <danielbernalherrera@gmail.com>
2021-11-04 12:14:39 -0500

    first commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "./Formulario.module.css";
+import PropTypes from "prop-types";
+import useSelect from "../hooks/useSelect";
+
+const Formulario = ({ guardarCategoria }) => {
+  const Opciones = [
+    { value: "general", label: "General" },
+    { value: "business", label: "Negocios" },
+    { value: "entertainment", label: "Entretenimiento" },
+    { value: "health", label: "Salud" },
+    { value: "science", label: "Ciencia" },
+    { value: "sports", label: "Deportes" },
+    { value: "technology", label: "Tecnología" },
+  ];
+
+  // Utilizar custom hook
+  const [categoria, SelectNoticias] = useSelect("general", Opciones);
+
+  // Cuando el usuario da submit, pasar categoria a app.js
+  const CargarNoticia = (e) => {
+    e.preventDefault();
+    guardarCategoria(categoria);
+  };
+
+  return (
+    <div className={`${styles.heading} row`}>
+      <div className="col s12 m8 offset-m2">
+        <form onSubmit={CargarNoticia}>
+          <h2 className={styles.heading}>Encuentro Noticias por Categoria</h2>
+
+          <SelectNoticias />
+
+          <div className="input-field col s12">
+            <input
+              type="submit"
+              className={`${styles["btn-block"]} btn-large amber darken-2`}
+              value="value"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+Formulario.propTypes = {
+  guardarCategoria: PropTypes.func.isRequired,
+};
+
+export default Formulario;
